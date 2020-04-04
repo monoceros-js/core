4f673a6ea9d6029d91f570e3a74ef9d1d064c2d7
Folkert-Jan van der Pol <folkertjanvanderpol@gmail.com>
2020-04-04 18:28:42 +0200

    Removed default callback from observer.create

--- src/utils/observer.js
+++ src/utils/observer.js
@@ -1,27 +1,10 @@
-export const createObserver = ({
-  root,
-  className,
-  threshold,
-  rootMargin,
-  cb,
-}) => {
+export const createObserver = ({ root, threshold, rootMargin }, callback) => {
   threshold = threshold || 0
   rootMargin = rootMargin || '0px'
   return new IntersectionObserver(
     entries => {
-      entries.forEach((entry, index) => {
-        const { isIntersecting, target } = entry
-        if (isIntersecting) {
-          if (!target.classList.contains(className)) {
-            target.classList.add(className)
-          }
-        } else {
-          if (target.classList.contains(className)) {
-            target.classList.remove(className)
-          }
-        }
-        if (cb) cb(entry, index)
-      })
+      if (!callback) return
+      callback(entries)
     },
     { root, threshold, rootMargin }
   )
